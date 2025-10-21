bf5895e583c857b99282f5821c5c0a2de7e5ba3b
VincentOosterom <vincent@oosteromstudio.nl>
2025-10-21 19:58:05 +0200

    opdracht 3 af

--- src/pages/BlogDetailPage/BlogDetailPage.jsx
+++ src/pages/BlogDetailPage/BlogDetailPage.jsx
@@ -1,23 +1,74 @@
 import './BlogDetailPage.css'
 import {Link, useParams} from "react-router-dom";
-import posts from "../../../public/data.json";
 import NavBar from "../../components/Navigatie/NavBar.jsx";
 import {formatDate} from "../../helpers/formatDate.js"
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 
 function BlogDetailPage() {
     const {id} = useParams();
-    const post = posts.find(post => post.id === parseInt(id));
+    const [post, setPost] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            try {
+                const response = await axios.get('/data.json');
+                const data = response.data;
+
+                const foundPost = data.find((item) => String(item.id) === id);
+
+                if (!foundPost) {
+                    throw new Error("Fout bij het ophalen van de post.");
+                }
+                console.log("Gevonden post", foundPost);
+                setPost(foundPost);
+            } catch (err) {
+                console.error(err);
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchPost();
+
+    }, [id]);
+
+
+    if (loading) {
+        return (
+            <>
+                <NavBar/>
+                <p>Bezig met laden...</p>
+            </>
+        );
+    }
+
+    if (error) {
+        return (
+            <>
+                <NavBar/>
+                <p style={{color: "red"}}>{error}</p>
+            </>
+        );
+    }
 
     if (!post) {
-        return <p>Post niet gevonden..</p>;
+        return (
+            <>
+                <NavBar/>
+                <p>Geen post gevonden.</p>
+            </>
+        );
     }
 
     return (
         <>
             <NavBar/>
             <section className="blog-detail">
-                <article key={post.id} className="blog-post">
+                <article className="blog-post">
                     <div className="blog-post-content">
                         <strong><h2>{post.title}</h2></strong>
                         <strong>{post.subtitle}</strong>
